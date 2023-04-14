5a3a633763958bb7fa9f4708c7a4ba7860855613
valera <vchichkins@gmail.com>
2023-04-14 20:56:18 +0300

    create game

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask import render_template, redirect, url_for
+from model import Game
+import random
+import time
+
+app = Flask(__name__)
+#app.config["game"] = Game(3, 3)
+#app.debug = True
+
+
+
+@app.route("/")
+def ferst():    
+    a = Game(5,5)
+    return render_template("index.html" )
+
+@app.route("/game", endpoint='game')
+def game():
+    mapG = Game().gameMap.tolist()
+    return render_template("game.html", gameMap= mapG)
+
+@app.route("/nextGen")
+def nextGen():
+    Game().gen_next()
+    return redirect(url_for('game'))
+
+if __name__ == "__main__":
+    app.run(host= "127.0.0.1", port = 8080)
---
+++ model.py
@@ -0,0 +1,63 @@
+import random
+import numpy as np
+
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class Game(metaclass = SingletonMeta):
+    def __init__(self, size_x= 3, size_y =3):
+        self.y = size_y
+        self.x = size_x
+        self.gameMap = self.gen_start_map()
+        
+
+    def gen_start_map(self):
+        game_map = np.zeros((self.y, self.x))
+        state = (0,1,-1)
+        for i in range(self.y):
+            for j in range(self.x):
+                game_map[i, j] = random.choice(state)
+        return game_map
+
+    def gen_next(self):
+        for i in range(self.y):
+            for j in range(self.x):
+                self.check_live(i,j)
+    
+    def check_live(self, pos_y, pos_x):
+        count_neighbour = 0
+
+        if pos_x + 1 < self.x:
+            if self.gameMap[pos_y, pos_x + 1] == 1:
+                count_neighbour += 1
+            
+
+        if pos_x - 1 >= 0:
+            if self.gameMap[pos_y, pos_x - 1] == 1:
+                count_neighbour += 1
+            
+        
+        if pos_y + 1 < self.y:
+            if self.gameMap[pos_y + 1, pos_x ] == 1:
+                count_neighbour += 1
+            
+        
+        if pos_y - 1 >= 0:
+            if self.gameMap[pos_y - 1, pos_x ] == 1:
+                count_neighbour += 1
+        
+        if  count_neighbour == 2 or count_neighbour == 3:
+            self.gameMap[pos_y, pos_x] = 1
+            return
+        
+        if self.gameMap[pos_y, pos_x] == 1:
+            self.gameMap[pos_y, pos_x] = -1
+            return
+        
+        self.gameMap[pos_y, pos_x] = 0
+        
